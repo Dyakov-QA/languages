89e31a619b97a006ae7b11021288018b59445654
Dyakov-QA <dyakov.serhey@gmail.com>
2020-01-14 10:44:07 +0200

    Completed test project

--- test_items.py
+++ test_items.py
@@ -1,6 +1,9 @@
+import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_button_exist(browser):
     browser.get(link)
-    browser.find_element_by_css_selector(".btn-add-to-basket")
+    time.sleep(10)
+    result = browser.find_element_by_css_selector("#add_to_basket_form")
+    assert len(result.text) > 0, "The button is not exist!"
